15a7104f37c8095a309eac8d207a9808c4b64621
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2022-10-06 16:18:12 -0700

    stuff

--- deployCommands.ts
+++ deployCommands.ts
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder, Routes } = require('discord.js');
 const { REST } = require('@discordjs/rest');
-var dotenv = require('dotenv');
-dotenv.config();
+const dotenv2 = require('dotenv');
+dotenv2.config();
 
 const commands = [
     new SlashCommandBuilder()
@@ -11,6 +11,26 @@ const commands = [
 		    option.setName('address')
 		    	.setDescription('The address you want to add')
 		    	.setRequired(true)),
+	new SlashCommandBuilder()
+        .setName('add_multiple_wallet')
+        .setDescription('Add multiple addresses to list of wallets to track')
+        .addStringOption(option =>
+		    option.setName('addresses')
+		    	.setDescription('The addresses you want to add, seperated with commas')
+		    	.setRequired(true)),
+	new SlashCommandBuilder()
+        .setName('view_wallets')
+        .setDescription('View list of currently tracked wallets'),
+	new SlashCommandBuilder()
+        .setName('remove_wallet')
+        .setDescription('Removed currently tracked wallet')
+        .addStringOption(option =>
+		    option.setName('address')
+		    	.setDescription('The address you want to remove')
+		    	.setRequired(true)),
+	new SlashCommandBuilder()
+        .setName('remove_all_wallets')
+        .setDescription('Removed currently tracked wallets')
 ]
 	.map(command => command.toJSON());
 
--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
+const AWS = require('aws-sdk');
 const { Client, GatewayIntentBits } = require('discord.js');
-var dotenv = require('dotenv');
+const dotenv = require('dotenv');
 dotenv.config();
+
+AWS.config.update({region: 'us-west-2'});
+var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -16,12 +20,96 @@ client.on('interactionCreate', async interaction => {
 	if (interaction.commandName === 'add_wallet') {
         let address = interaction.options.getString('address');
         let user = interaction.user.id;
-
         if (/^0x[a-fA-F0-9]{40}$/.test(address)) {
-            await interaction.reply({ content: `${user}, ${address}`, ephemeral: true });
+            // getting current addresses
+            let getParams = {
+                Key: {
+                 "userId": {S: `${user}`}, 
+                },
+                TableName: "userAddresses"
+            };
+
+            let currentAddresses;
+
+            ddb.getItem(getParams, function(err, data) {
+                currentAddresses = data.Item.addresses.S;
+                if (err) {
+                    console.log("Error", err);
+                } else {
+                    console.log("Success", data);
+                }
+            });
+            //
+
+
+            // adding addresses
+            let params = {
+                Item: {
+                 "userId": {S: `${user}`}, 
+                 "addresses": {S: `${currentAddresses}, ${address}`}
+                },
+                TableName: "userAddresses"
+            };
+
+              // Call DynamoDB to add the item to the table
+              ddb.putItem(params, function(err, data) {
+                if (err) {
+                  console.log("Error", err);
+                  interaction.reply({ content: 'error', ephemeral: true });
+                } else {
+                  console.log("Success", data);
+                  interaction.reply({ content: 'success', ephemeral: true });
+                }
+              });
         } else {
             await interaction.reply({ content: 'invalid address provided', ephemeral: true });
         }
+	} else if (interaction.commandName === 'add_multiple_wallet') {
+        let address = interaction.options.getString('address');
+        let user = interaction.user.id;
+        if (/^0x[a-fA-F0-9]{40}$/.test(address)) {
+            let params = {
+                Item: {
+                 "userId": {S: `${user}`}, 
+                 "addresses": {S: `${address}`}
+                },
+                TableName: "userAddresses"
+            };
+
+              // Call DynamoDB to add the item to the table
+              ddb.putItem(params, function(err, data) {
+                if (err) {
+                  console.log("Error", err);
+                  interaction.reply({ content: 'error', ephemeral: true });
+                } else {
+                  console.log("Success", data);
+                  interaction.reply({ content: 'success', ephemeral: true });
+                }
+              });
+        } else {
+            await interaction.reply({ content: 'invalid address provided', ephemeral: true });
+        }
+	} else if (interaction.commandName === 'view_wallets') {
+        let user = interaction.user.id;
+        let params = {
+            Key: {
+             "userId": {S: `${user}`}, 
+            },
+            TableName: "userAddresses"
+        };
+
+        // Call DynamoDB to read the item from the table
+        ddb.getItem(params, function(err, data) {
+            let justAddress = data.Item.addresses.S;
+            if (err) {
+                console.log("Error", err);
+                interaction.reply({ content: 'error', ephemeral: true });
+            } else {
+            
+                console.log("Success", data);
+                interaction.reply({ content: `${justAddress}`, ephemeral: true });
+            }
+        });
 	}
 });
 
