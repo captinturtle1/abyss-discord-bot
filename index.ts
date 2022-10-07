333b42c19257c43c082e87b7b41cd3e23c144141
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2022-10-07 11:25:29 -0700

    add_wallet
    command now lets you add multiple at a time spaced with commas, and will add upon previous list of addresses instead of replacing

--- index.ts
+++ index.ts
@@ -18,34 +18,54 @@ client.on('interactionCreate', async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
 	if (interaction.commandName === 'add_wallet') {
+    let user = interaction.user.id;
+
+    // params for getting current address(s)
+    let getParams = {
+      Key: {
+       "userId": {S: `${user}`}, 
+      },
+      TableName: "userAddresses"
+    }
+    
+    ddb.getItem(getParams, function(err, data) {
+      if (err) {
+        console.log("Error", err);
+        interaction.reply({ content: 'error', ephemeral: true });
+      } else {
+        // gets user inputted address(s) and converts to array
         let address = interaction.options.getString('address');
         let nospace = address.replace(/\s+/g, '');
         let myArray = nospace.split(",");
-        let user = interaction.user.id;
-        for (let i = 0; i < myArray.length; i++) {
-          console.log(myArray[i]);
-          if (!/^0x[a-fA-F0-9]{40}$/.test(myArray[i])) {
-            await interaction.reply({ content: `${myArray[i]} is not valid`, ephemeral: true });
+
+        let newArray;
+
+        // checks if there are existing addresses
+        if (Object.keys(data).length !== 0) {
+          //if there are existing addresses
+          let currentAddresses = data.Item.addresses.S.split(",");
+          console.log(`current addresses is defined, ${currentAddresses}`);
+          newArray = currentAddresses.concat(myArray);
+          console.log(`${newArray} is the newArray after adding`);
+        } else {
+          // if no existing addresses
+          newArray = myArray;
+        }
+
+        // removes any duplicated addresses from new address array
+        let noDupe = newArray.filter((item, index) => newArray.indexOf(item) === index);
+
+        // scans thru array to make sure all items are valid addresses
+        for (let i = 0; i < noDupe.length; i++) {
+          console.log(`${noDupe[i]} is data in ${i} position`);
+          if (!/^0x[a-fA-F0-9]{40}$/.test(noDupe[i])) {
+            interaction.reply({ content: `${noDupe[i]} is not valid`, ephemeral: true });
             return;
           }
         }
-
-        let noDupe = myArray.filter((item, index) => myArray.indexOf(item) === index);
         
-        // getting current addresses
-        let getParams = {
-            Key: {
-             "userId": {S: `${user}`}, 
-            },
-            TableName: "userAddresses"
-        }
-        ddb.getItem(getParams, function(err, data) {
-            if (err) {
-                console.log("Error", err);
-            } else {
-                console.log("Success", data);
-            }
-        })
+
+        // users item db entry
         let params = {
             Item: {
              "userId": {S: `${user}`}, 
@@ -53,6 +73,7 @@ client.on('interactionCreate', async interaction => {
             },
             TableName: "userAddresses"
         }
+
         // Call DynamoDB to add the item to the table
         ddb.putItem(params, function(err, data) {
           if (err) {
@@ -63,6 +84,9 @@ client.on('interactionCreate', async interaction => {
             interaction.reply({ content: 'success', ephemeral: true });
           }
         });
+      }
+    })
+
 	} else if (interaction.commandName === 'view_wallets') {
         let user = interaction.user.id;
         let params = {
