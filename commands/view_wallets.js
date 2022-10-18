7b55b9191fff36feeff1668fe35fdf6afb9e7888
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2022-10-17 22:49:21 -0700

    Update view_wallets.js
    view wallet now sends in blocks of 30 addresses if there are more than 30 addresses to prevent hitting discord message limit

--- commands/view_wallets.js
+++ commands/view_wallets.js
@@ -10,19 +10,48 @@ export default function view_wallets(interaction, ddb) {
     };
     
     // Call DynamoDB to read the item from the table
-    ddb.getItem(params, function(err, data) {
+    ddb.getItem(params, async function(err, data) {
         if (err) {
             console.log("Error", err);
-            interaction.reply({ content: 'error', ephemeral: true });
+            await interaction.reply({ content: 'error', ephemeral: true });
         } else {
             if (Object.keys(data).length !== 0) {
-                let justAddress = data.Item.addresses.S;
-                let nospace = justAddress.replace(/,/g, '\n');
                 console.log("Success", data);
-                interaction.reply({ content: `${codeBlock(nospace)}`, ephemeral: true });
+                let addressArray = data.Item.addresses.S.split(',');
+                console.log('addressArray.length', addressArray.length)
+                if (addressArray.length > 30) {
+                    let addressRemaineder = addressArray.length % 30;
+                    console.log('addressRemaineder', addressRemaineder);
+                    let addressLoopAmount = (addressArray.length - addressRemaineder) / 30
+                    console.log('addressLoopAmount', addressLoopAmount);
+                    let hasReplied = false;
+                    for (let i = 0; i < addressLoopAmount; i++) {
+                        let currentBlock = '';
+                        for (let j = 0; j < 30; j++) {
+                            currentBlock = currentBlock + '\n' + addressArray[j];
+                        }
+                        if (hasReplied) {
+                            await interaction.followUp({ content: `${codeBlock(currentBlock)}`, ephemeral: true });
+                        } else {
+                            await interaction.reply({ content: `${addressArray.length} addresses being tracked ${codeBlock(currentBlock)}`, ephemeral: true });
+                            hasReplied = true;
+                        }
+                    }
+                    if (addressRemaineder > 0) {
+                        let currentBlock = '';
+                        for (let j = 0; j < addressRemaineder; j++) {
+                            currentBlock = currentBlock + '\n' + addressArray[j + (addressLoopAmount * 30)];
+                        }
+                        await interaction.followUp({ content: `${codeBlock(currentBlock)}`, ephemeral: true });
+                    }
+                    //console.log(addressArray);
+                } else {
+                    let formattedAddresses = data.Item.addresses.S.replace(/,/g, '\n');
+                    await interaction.reply({ content: `${addressArray.length} address(s) being tracked ${codeBlock(formattedAddresses)}`, ephemeral: true });
+                }
             } else {
                 console.log('no wallets found');
-                interaction.reply({ content: 'no wallets found', ephemeral: true });
+                await interaction.reply({ content: 'no wallets found', ephemeral: true });
             }
         }
     });
