0d2eee52a0915b7337a7722a05883aab2dd68000
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2022-10-17 22:49:57 -0700

    Update remove_wallet.js
    removed listing each address that was removed and now just shows the number of wallets removed

--- commands/remove_wallet.js
+++ commands/remove_wallet.js
@@ -1,3 +1,5 @@
+import { codeBlock } from 'discord.js';
+
 export default function remove_all_wallets(interaction, ddb) {
     let user = interaction.user.id;
     let getParams = {
@@ -7,7 +9,7 @@ export default function remove_all_wallets(interaction, ddb) {
       TableName: "userAddresses"
     };
     
-    ddb.getItem(getParams, function(err, data) {
+    ddb.getItem(getParams, async function(err, data) {
       if (err) {
           console.log("Error", err);
           interaction.reply({ content: 'error', ephemeral: true });
@@ -17,20 +19,17 @@ export default function remove_all_wallets(interaction, ddb) {
           let nospace = address.replace(/\s+/g, '');
           let addressArray = nospace.split(",");
           let currentAddresses = data.Item.addresses.S.split(",");
-          let responseText = "";
+          let amountRemoved = 0;
           for (let i = 0; i < addressArray.length; i++) {
             if (currentAddresses.includes(addressArray[i])) {
               let found = currentAddresses.findIndex(element => element === addressArray[i]);
-              console.log(found);
-              console.log(`${currentAddresses} is before splice`);
               currentAddresses.splice(found, 1);
-              responseText = responseText + `${addressArray[i]} removed\n`;
-              console.log(`${currentAddresses} is after splice`);
+              amountRemoved++;
             } else {
               console.log(`${addressArray[i]} not being tracked`);
-              responseText = responseText + `${addressArray[i]} not being tracked\n`;
             };
           }
+
           let params = {
             Item: {
              "userId": {S: `${user}`}, 
@@ -40,17 +39,22 @@ export default function remove_all_wallets(interaction, ddb) {
           }
         
           // Call DynamoDB to add the item to the table
-          ddb.putItem(params, function(err, data) {
+          ddb.putItem(params, async function(err, data) {
             if (err) {
               console.log("Error", err);
-              interaction.reply({ content: 'error', ephemeral: true });
+              await interaction.reply({ content: 'error', ephemeral: true });
             } else {
               console.log("Success", data);
-              interaction.reply({ content: `${responseText}`, ephemeral: true });
+              if (amountRemoved == 0) {
+                await interaction.reply({ content: 'no addresses provided', ephemeral: true });
+              } else {
+                await interaction.reply({ content: `removed ${amountRemoved} address(s)`, ephemeral: true });
+              }
+              
             }
           });
         } else {
-          interaction.reply({ content: 'no wallets being tracked', ephemeral: true });
+          await interaction.reply({ content: 'no wallets being tracked', ephemeral: true });
         };
       }
     });
